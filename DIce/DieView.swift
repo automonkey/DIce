8f9620e5924da22d66624de52caf83c9cdb53dba
Will Benyon <gitster@benyon.io>
2015-05-02 11:13:45 +0100

    temp: drawing 6 dots

--- DIce/DieView.swift
+++ DIce/DieView.swift
@@ -18,7 +18,34 @@ class DieView : UIView {
 
     override func drawRect(rect: CGRect) {
         let dieFaceRect = CGRectInset(rect, borderWidth, borderWidth)
-        drawSpot(dieFaceRect)
+
+        for dotRect in getSpotRects(rect) {
+            drawSpot(dotRect)
+        }
+    }
+
+    private func getSpotRects(rect: CGRect) -> [CGRect] {
+        let cols = 3
+        let rows = 3
+
+        var dotRects = [CGRect]()
+
+        let dots:[(CGFloat,CGFloat)] = [
+            (0,0),(2,0),
+            (0,1),(1,1),(2,1),
+            (0,2),(2,2)
+        ]
+
+        let dotRectWidth:CGFloat = rect.width/3
+        let dotRectHeight:CGFloat = rect.height/3
+
+        for dot in dots {
+            let xorigin = dot.0 * dotRectWidth
+            let yorigin = dot.1 * dotRectHeight
+            dotRects.append(CGRect(x: xorigin, y: yorigin, width: dotRectWidth, height: dotRectHeight))
+        }
+
+        return dotRects
     }
 
     private func drawSpot(rect: CGRect) {
